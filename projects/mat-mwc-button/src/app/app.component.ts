6b61e862a845a925e2fa47be75deee6c52988ffd
Jeremy Elbourn <jelbourn@google.com>
2020-04-14 15:18:49 -0700

    Add button

---
+++ projects/mat-mwc-button/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<mwc-button>Click me</mwc-button>`,
+  styles: []
+})
+export class AppComponent { }
