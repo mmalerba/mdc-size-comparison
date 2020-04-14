92c291e99958f11e4351a7255debfd71c6c8d034
Jeremy Elbourn <jelbourn@google.com>
2020-04-14 16:35:51 -0700

    Add slide-toggle

---
+++ projects/mat-mwc-slide-toggle/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<mwc-formfield label="Check me"><mwc-switch></mwc-switch></mwc-formfield>`,
+  styles: []
+})
+export class AppComponent { }
